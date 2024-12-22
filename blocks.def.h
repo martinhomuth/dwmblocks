bba68a213d493d19518e5167a3e2c80876882384
Martin Homuth <martin.homuth@emlix.com>
2024-12-22 21:12:11 +0100

    update blocks
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"",	"~/.dwm/status/battery.sh",	10,	26},
+	{"",	"~/.dwm/status/cpu.sh",	1,	11},
+	{"",	"~/.dwm/status/nettraffic.sh",	1,	8},
+	{"",	"~/.dwm/status/ram.sh",		10,	6},
+	{"",	"~/.dwm/status/volume.sh",	1,	7},
+	{"",	"~/.dwm/status/vpn.sh",	1,	7},
+	{"",	"~/.dwm/status/date.sh",	180,	12},
+	{"",	"~/.dwm/status/time.sh",	1,	5},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
